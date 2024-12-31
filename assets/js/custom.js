bddd6a0dd1b111e14e8b935ca5d6c7ecdd4a3d4c
cryptorichyrich <fxwisdom1@gmail.com>
2024-12-31 11:44:09 +0700

    daftar isi

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const toc = document.getElementById("toc");
+    const mainNavbar = document.getElementById("main-navbar");
     let isCollapsed = false;
   
     // Handle scroll behavior
@@ -19,6 +20,28 @@ document.addEventListener("DOMContentLoaded", () => {
         if (event.target.textContent.toLowerCase().includes("daftar isi")) {
           toc.classList.toggle("collapsed");
         }
-      });      
+      }); 
+      
+    // Smooth scroll to headers with offset
+    toc.addEventListener("click", (event) => {
+        const target = event.target;
+        if (target.tagName === "A" && target.hash) {
+        event.preventDefault();
+        const header = document.querySelector(target.hash);
+        const navbarHeight = mainNavbar.offsetHeight;
+        const tocHeight = toc.offsetHeight;
+
+        if (header) {
+            const offset = navbarHeight + tocHeight;
+            const headerPosition = header.getBoundingClientRect().top + window.scrollY;
+            const scrollPosition = headerPosition - offset;
+
+            window.scrollTo({
+            top: scrollPosition,
+            behavior: "smooth",
+            });
+        }
+        }
+    });      
   });
   
